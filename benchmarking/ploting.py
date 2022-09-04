83f3bd2937a6ec5d10bc076acf61ed85f2bac3ae
Thomas Klotz <thomas.klotz98@gmail.com>
2022-09-04 21:25:19 +0200

    refactoring

--- benchmarking/ploting.py
+++ benchmarking/ploting.py
@@ -9,7 +9,9 @@ from benchmarking import MeasurementParameters, get_limits
 
 
 def create_plots(measurement_parameters):
-    data, files = _get_data_vector(measurement_parameters, "vector-operations")
+    # TODO: split up into several functions
+
+    data, files = _get_data_vectorization(measurement_parameters, "vector-operations")
     energies = list()
     times = list()
 
@@ -48,7 +50,9 @@ def create_plots(measurement_parameters):
     plt.show()
 
 
-def _get_data_vector(parameters, benchmark_name):
+def _get_data_vectorization(parameters, benchmark_name):
+
+    # TODO: put following five lines into a function
     path = "outputs/"
     folders = [folder for folder in os.listdir(path) if parameters.limit_type in folder]
     folders.sort()
@@ -58,19 +62,11 @@ def _get_data_vector(parameters, benchmark_name):
     files_vectorization = dict()
     plot_data = dict()
 
-    files_vectorization["1"] = \
-        [file for file in os.listdir(path) if "_vectorization-size-1_" in file and "_vector-size-2048_" in file]
-    files_vectorization["2"] = \
-        [file for file in os.listdir(path) if "_vectorization-size-2_" in file and "_vector-size-2048_" in file]
-    files_vectorization["4"] = \
-        [file for file in os.listdir(path) if "_vectorization-size-4_" in file and "_vector-size-2048_" in file]
-    files_vectorization["8"] = \
-        [file for file in os.listdir(path) if "_vectorization-size-8_" in file and "_vector-size-2048_" in file]
-    files_vectorization["16"] = \
-        [file for file in os.listdir(path) if "_vectorization-size-16_" in file and "_vector-size-2048_" in file]
+    for vectorization_size in parameters.vectorization_sizes:
+        files_vectorization[str(vectorization_size)] = \
+            [file for file in os.listdir(path) if f"_vectorization-size-{vectorization_size}_" in file and "_vector-size-2048_" in file]
 
     for key, value in files_vectorization.items():
-
         files_vectorization[key].sort()
 
         for file in files_vectorization[key]:
@@ -90,77 +86,6 @@ def _get_measurement(tokens, unit):
     measurement_index = tokens.index(unit) - 1
     measurement = tokens[measurement_index]
     return float(measurement)
-
-
-def _get_data_from_file(benchmark_name):
-    plot_files = []
-    path = "plot-data/"
-    folders = [f.path for f in os.scandir(path) if f.is_dir()]
-
-    folders.sort()
-    path = folders[-1]
-
-    plot_files += [file for file in os.listdir(path) if benchmark_name in file]
-
-    plot_files.sort()
-    plot_file = plot_files[-1]
-
-    data = []
-    with open(f"{path}/{plot_file}", 'r') as file:
-        csvreader = csv.reader(file)
-        header = next(csvreader)
-        for row in csvreader:
-            data.append(row)
-
-    return data
-
-
-def _get_measurements(data, measurement_parameters):
-    energy_per_benchmark_per_limit = {}
-    time_per_benchmark_per_limit = {}
-
-    start_index = 0
-    end_index = measurement_parameters.iterations
-    for benchmark in range(0, len(measurement_parameters.benchmark_names) - 1):
-        for limit in measurement_parameters.limits:
-            energy_per_benchmark_per_limit[benchmark, limit], time_per_benchmark_per_limit[benchmark, limit] = \
-                _get_measurements_aux(data, start_index, end_index)
-
-            start_index = end_index
-            end_index = start_index + measurement_parameters.iterations
-
-    return energy_per_benchmark_per_limit, time_per_benchmark_per_limit
-
-
-def _get_measurements_aux(data, start_index, end_index):
-    energy_measurements_per_benchmark_per_limit = []
-    time_measurements_per_benchmark_per_limit = []
-
-    for index in range(start_index, end_index):
-        energy_measurements_per_benchmark_per_limit.append(data[index][0])
-        time_measurements_per_benchmark_per_limit.append(data[index][1])
-
-    return energy_measurements_per_benchmark_per_limit, time_measurements_per_benchmark_per_limit
-
-
-def _get_mean_measurements(measurements_per_benchmark_per_limit, measurement_parameters):
-    mean_measurements = {}
-
-    for benchmark in range(0, len(measurement_parameters.benchmark_names) - 1):
-        mean_measurements[benchmark] = \
-            _get_mean_measurements_aux(measurements_per_benchmark_per_limit, measurement_parameters, benchmark)
-
-    return mean_measurements
-
-
-def _get_mean_measurements_aux(measurements_per_benchmark_per_limit, measurement_parameters, benchmark):
-    mean_measurements = []
-
-    for limit in measurement_parameters.limits:
-        measurements_float = [float(i) for i in measurements_per_benchmark_per_limit[benchmark, limit]]
-        mean_measurements.append(mean(measurements_float))
-
-    return mean_measurements
 
 
 def _create_scatter_plot(x_size, y_size, position, title, x_label, y_label, x, y):
