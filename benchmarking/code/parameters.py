529988c2376b50bc094bca05f9c7a3f9df15028e
Thomas Klotz <thomas.klotz98@gmail.com>
2023-01-05 12:20:22 +0100

    added instruction set parameter

--- benchmarking/code/parameters.py
+++ benchmarking/code/parameters.py
@@ -1,7 +1,7 @@
 class Parameters:
     # add new parameters here, in benchmarking.save_config() and in data_processing.get_config()
     def __init__(self, benchmark_names, start_time, iterations, limit_type, limits, thread_counts, vectorization_sizes, vector_sizes,
-                 datatypes, map_sizes, optimization_flags):
+                 datatypes, map_sizes, optimization_flags, instruction_sets):
         self.benchmark_names = benchmark_names
         self.start_time = start_time
         self.iterations = iterations
@@ -13,3 +13,4 @@ class Parameters:
         self.datatypes = datatypes
         self.map_sizes = map_sizes
         self.optimization_flags = optimization_flags
+        self.instruction_sets = instruction_sets
