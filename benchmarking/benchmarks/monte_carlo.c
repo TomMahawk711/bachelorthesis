dad11e3b0788d6f745185047d5312251c4e501f5
Thomas Klotz <thomas.klotz98@gmail.com>
2023-01-16 16:24:07 +0100

    updated benchmark

--- benchmarking/benchmarks/monte_carlo.c
+++ benchmarking/benchmarks/monte_carlo.c
@@ -1,68 +1,49 @@
-#include <omp.h>
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
-void* calculate_partial_monte_carlo(void*);
+int main(int argc, char** argv){
 
-struct thread_args {
-	unsigned int seed;
-	long* fract_n;
-};
+    int n = 0;
+    int num_threads = 1;
 
-int main(int argc, char** argv) {
+    if(argc < 3){
+        printf("Usage: ./monte_carlo.out <number_of_calculations> <number_of_threads>\n");
+        return EXIT_FAILURE;
+    } else{
+        n = atol(argv[1]);
+        num_threads = atol(argv[2]);
+    }
 
-	int number_of_threads = 1;
-	long n = 0;
+    unsigned int seed = 0;
+    long total_inside = 0;
+    omp_set_num_threads(num_threads);
+    double start_time = omp_get_wtime();
 
-	if(argc < 3) {
-		printf("Usage: ./monte_carlo_par.out <n> <number_of_threads>\n");
-		return EXIT_FAILURE;
-	} else {
-		number_of_threads = atoi(argv[2]);
-		n = atol(argv[1]);
-	}
+    #pragma omp parallel private(seed)
+    {
+        long partial_inside = 0;
+        seed = time(NULL);
+        #pragma omp for schedule(static)
+        for(int i = 0; i < n; i++){
+            float x = ((float) rand_r(&seed) / (RAND_MAX));
+            float y = ((float) rand_r(&seed) / (RAND_MAX));
+            if((x*x + y*y)<= 1){
+                partial_inside++;
+            }
+        }
 
-	long fract_n = n / number_of_threads;
-	long total_inside_points = 0;
-	pthread_t threads[number_of_threads - 1];
-	void* ret_vals[number_of_threads];
-	struct thread_args t_args[number_of_threads];
+        #pragma omp for schedule(static)
+        for(int i = 0; i<num_threads; i++){
+            #pragma omp atomic
+                total_inside+=partial_inside;
+        }
+    }
 
-	for(int i = 0; i < number_of_threads - 1; i++) {
-		t_args[i].fract_n = &fract_n;
-		t_args[i].seed = time(NULL);
-		pthread_create(&threads[i], NULL, calculate_partial_monte_carlo, (void*)&t_args[i]);
-	}
+    double end_time = omp_get_wtime();
+    printf("sum: %f time: %2.6f \n", 4 * (float) total_inside / n, end_time-start_time);
 
-	t_args[number_of_threads-1].fract_n = &fract_n;
-	t_args[number_of_threads-1].seed = time(NULL);
-	ret_vals[number_of_threads - 1] = calculate_partial_monte_carlo((void*)&t_args);
-	total_inside_points += *(long*)ret_vals[number_of_threads - 1];
-	free(ret_vals[number_of_threads - 1]);
 
-	for(int i = 0; i < number_of_threads - 1; i++) {
-		pthread_join(threads[i], &ret_vals[i]);
-		total_inside_points += *(long*)ret_vals[i];
-		free(ret_vals[i]);
-	}
-
-	return EXIT_SUCCESS;
-}
-
-void* calculate_partial_monte_carlo(void* n_part) {
-	struct thread_args args = *(struct thread_args*)n_part;
-	long total_inside = 0;
-	for(int i = 0; i < *args.fract_n; i++) {
-		float x = ((float)rand_r(&args.seed) / (RAND_MAX));
-		float y = ((float)rand_r(&args.seed) / (RAND_MAX));
-		if((x * x + y * y) <= 1) {
-			total_inside++;
-		}
-	}
-
-	long* retptr = malloc(sizeof(long));
-	*retptr = total_inside;
-	return (void*)retptr;
+    return EXIT_SUCCESS;
 }
