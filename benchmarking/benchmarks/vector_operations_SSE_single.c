b4611dd3f30441af762197319485b1688cf7bf8b
Thomas Klotz <thomas.klotz98@gmail.com>
2023-01-30 19:49:47 +0100

    added multi threading in vector benchmarks

--- benchmarking/benchmarks/vector_operations_SSE_single.c
+++ benchmarking/benchmarks/vector_operations_SSE_single.c
@@ -5,17 +5,19 @@
 #include <malloc.h>
 #include "vector_operations_aux.h"
 
-void calculate_array(float*, float*, float*, int, int);
+void calculate_array(float*, float*, float*, int, int, int);
 
 int main(int argc, char** argv){
 
     int const repetitions = 1e6;
     int size = 0;
+    int num_threads = 1;
 
 	if(argc < 2) {
-		printf("usage: task3.out <vector_size>\n");
+		printf("usage: task3.out <vector_size> <num_threads>\n");
 		return EXIT_FAILURE;
 	} else {
+	    num_threads = atol(argv[2]);
 		size = atol(argv[1]);
 	}
 
@@ -28,7 +30,7 @@ int main(int argc, char** argv){
 	init_array_single_precision(c, size, 2);
 
 	//double start_time = omp_get_wtime();
-	calculate_array(a, b, c, size, repetitions);
+	calculate_array(a, b, c, size, repetitions, num_threads);
 	//double end_time = omp_get_wtime();
 
     //printf("time: %f seconds\n", end_time - start_time);
@@ -42,12 +44,15 @@ int main(int argc, char** argv){
 	return EXIT_SUCCESS;
 }
 
-void calculate_array(float* a, float* b, float* c, int size, int repetitions){
+void calculate_array(float* a, float* b, float* c, int size, int repetitions, int num_threads){
     __m128 a_128;
 	__m128 b_128;
 	__m128 c_128;
 
+    omp_set_num_threads(num_threads);
+#pragma omp parallel
 	for(int run = 0; run < repetitions; ++run) {
+#pragma omp for
 		for(int i = 0; i < size; i += 4) {
 			a_128 = _mm_load_ps(&a[i]);
 			b_128 = _mm_load_ps(&b[i]);
