986e42bcfd42fc86c8abd33f7d6d0c296dffec42
Thomas Klotz <thomas.klotz98@gmail.com>
2023-02-01 18:22:56 +0100

    added stream array size parameter

--- benchmarking/code/parameters.py
+++ benchmarking/code/parameters.py
@@ -2,7 +2,7 @@ class Parameters:
     # add new parameters here, in benchmarking.save_config() and in data_processing.get_config()
     # TODO: check if different vector sizes are relevant
     def __init__(self, benchmark_names, start_time, iterations, limit_type, limits, thread_counts, vectorization_sizes, vector_sizes,
-                 precisions, optimization_flags, instruction_sets):
+                 precisions, optimization_flags, instruction_sets, stream_array_sizes):
         self.benchmark_names = benchmark_names
         self.start_time = start_time
         self.iterations = iterations
@@ -14,3 +14,4 @@ class Parameters:
         self.precisions = precisions
         self.optimization_flags = optimization_flags
         self.instruction_sets = instruction_sets
+        self.stream_array_sizes = stream_array_sizes
