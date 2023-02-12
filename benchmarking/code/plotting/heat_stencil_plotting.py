7c1a1f1e6dee7e1b296bc3fe59b11b877919cde0
Thomas Klotz <thomas.klotz98@gmail.com>
2023-02-12 17:59:12 +0100

    updated plotting

--- benchmarking/code/plotting/heat_stencil_plotting.py
+++ benchmarking/code/plotting/heat_stencil_plotting.py
@@ -2,9 +2,23 @@ from data_processing import process, get_config
 from plotting_templates import create_bar_plot, create_heatmap, create_scatter_plot
 
 
-# TODO
 def _create_heat_stencil_plots(folder_name):
-    pass
+    parameters = get_config(folder_name)
+    benchmark_name = "monte-carlo"
+    grouping_metric = parameters.limits
+    energies, times = process(parameters, benchmark_name, folder_name, grouping_metric)
+
+    powers = [energy / time for energy, time in zip(energies, times)]
+
+    energies_data = [(grouping_metric, energies)]
+    times_data = [(grouping_metric, times)]
+    powers_data = [(grouping_metric, powers)]
+    energies_times_data = [(times, energies)]
+
+    create_scatter_plot(energies_data, "frequencies", "energy []", "energy consumption heat stencil", folder_name)
+    create_scatter_plot(times_data, "frequencies", "time", "time heat stencil", folder_name)
+    create_scatter_plot(powers_data, "frequencies", "power", "power consumption heat stencil", folder_name)
+    create_scatter_plot(energies_times_data, "times", "energies", "energy/time heat stencil", folder_name)
 
 
 if __name__ == "__main__":
--- benchmarking/code/plotting/monte_carlo_plotting.py
+++ benchmarking/code/plotting/monte_carlo_plotting.py
@@ -8,13 +8,17 @@ def _create_monte_carlo_plots(folder_name):
     grouping_metric = parameters.limits
     energies, times = process(parameters, benchmark_name, folder_name, grouping_metric)
 
-    mean_power = [e / t for e, t in zip(energies, times)]
+    powers = [energy / time for energy, time in zip(energies, times)]
 
     energies_data = [(grouping_metric, energies)]
     times_data = [(grouping_metric, times)]
+    powers_data = [(grouping_metric, powers)]
+    energies_times_data = [(times, energies)]
 
-    create_scatter_plot(energies_data, "frequencies", "energies", "energy consumption monte carlo", folder_name)
-    create_scatter_plot(times_data, "frequencies", "energies", "energy consumption monte carlo", folder_name)
+    create_scatter_plot(energies_data, "frequencies", "energy []", "energy consumption monte carlo", folder_name)
+    create_scatter_plot(times_data, "frequencies", "time", "time monte carlo", folder_name)
+    create_scatter_plot(powers_data, "frequencies", "power", "power consumption monte carlo", folder_name)
+    create_scatter_plot(energies_times_data, "times", "energies", "energy/time monte carlo", folder_name)
 
 
 if __name__ == "__main__":
