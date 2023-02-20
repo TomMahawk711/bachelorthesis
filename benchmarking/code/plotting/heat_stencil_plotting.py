82bada0c678407ada7608a93276af7f1f0994240
Thomas Klotz <thomas.klotz98@gmail.com>
2023-02-20 20:43:14 +0100

    update plotting

--- benchmarking/code/plotting/heat_stencil_plotting.py
+++ benchmarking/code/plotting/heat_stencil_plotting.py
@@ -2,73 +2,141 @@ from data_processing import process, get_config
 from plotting_templates import create_bar_plot, create_heatmap, create_scatter_plot
 
 
-def _create_heat_stencil_plots():
-    folder_name = "i7-3770"
-    parameters = get_config(folder_name)
+def _create_heat_stencil_plots_by_thread_counts():
     benchmark_name = "heat-stencil"
+
+    parameters = get_config("i7-3770")
     grouping_metric = parameters.thread_counts
-    energies, times = process(parameters, benchmark_name, folder_name, grouping_metric, frequency=2100)
-    powers = [energy / time for energy, time in zip(energies, times)]
+    i7_energies, i7_times = process(parameters, benchmark_name, "i7-3770", grouping_metric, frequency=2100)
+    i7_powers = [energy / time for energy, time in zip(i7_energies, i7_times)]
 
-    energies_data = [(grouping_metric, energies)]
-    times_data = [(grouping_metric, times)]
-    powers_data = [(grouping_metric, powers)]
-    energies_times_data = [(times, energies)]
-
-    #create_scatter_plot(energies_data, "frequencies", "energy [J]", "energy consumption heat stencil", folder_name)
-    #create_scatter_plot(times_data, "frequencies", "time", "time heat stencil", folder_name)
-    #create_scatter_plot(powers_data, "frequencies", "power", "power consumption heat stencil", folder_name)
-    #create_scatter_plot(energies_times_data, "times", "energies", "energy/time heat stencil", folder_name)
+    i7_energies_data = [(grouping_metric, i7_energies)]
+    i7_times_data = [(grouping_metric, i7_times)]
+    i7_powers_data = [(grouping_metric, i7_powers)]
+    i7_energies_times_data = [(i7_times, i7_energies)]
 
     r7_parameters = get_config("R7-5800X")
-    r7_grouping_metric = r7_parameters.limits
+    r7_grouping_metric = r7_parameters.thread_counts
     r7_energies, r7_times = process(r7_parameters, benchmark_name, "R7-5800X", r7_grouping_metric, frequency=2200)
     r7_powers = [energy / time for energy, time in zip(r7_energies, r7_times)]
 
     r7_energies_data = [(r7_grouping_metric, r7_energies)]
     r7_times_data = [(r7_grouping_metric, r7_times)]
+    r7_powers_data = [(grouping_metric, r7_powers)]
+    r7_energies_times_data = [(r7_times, r7_energies)]
 
-    # create_scatter_plot(r7_energies_data, "frequencies", "energy [J]", "energy consumption heat stencil", ["R7 5800X"], "upper left")
-    # create_scatter_plot(times_data, "frequencies", "time", "time heat stencil", folder_name)
-    # create_scatter_plot(powers_data, "frequencies", "power", "power consumption heat stencil", folder_name)
-    # create_scatter_plot(energies_times_data, "times", "energies", "energy/time heat stencil", folder_name)
+    both_energies_data = [(grouping_metric, i7_energies), (r7_grouping_metric, r7_energies)]
+    both_times_data = [(grouping_metric, i7_times), (r7_grouping_metric, r7_times)]
+    both_powers_data = [(grouping_metric, i7_powers), (r7_grouping_metric, r7_powers)]
+    both_energies_times_data = [(i7_times, i7_energies), (r7_times, r7_energies)]
 
-    both_energies_data = [(grouping_metric, energies), (r7_grouping_metric, r7_energies)]
-    both_times_data = [(grouping_metric, times), (r7_grouping_metric, r7_times)]
-    both_powers_data = [(grouping_metric, powers), (r7_grouping_metric, r7_powers)]
-    both_energies_times_data = [(times, energies), (r7_times, r7_energies)]
-
-    # create_scatter_plot(both_energies_data, "frequencies [MHz]", "energy [J]", "energy consumption heat stencil", ["i7 3770", "R7 5800X"], "upper left")
-    #create_scatter_plot(both_times_data, "frequencies [MHz]", "time [s]", "time heat stencil", ["i7 3770", "R7 5800X"], "upper right")
-    create_scatter_plot(both_powers_data, "times [s]", "power [W]", "power heat stencil", ["i7 3770", "R7 5800X"], "upper left")
-    #create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time heat stencil", ["i7 3770", "R7 5800X"], "upper left")
+    create_scatter_plot(both_energies_data, "thread_count", "energy [J]", "energy consumption heat stencil", ["i7 3770", "R7 5800X"], "upper left")
+    create_scatter_plot(both_times_data, "thread count", "time [s]", "time heat stencil", ["i7 3770", "R7 5800X"], "upper right")
+    create_scatter_plot(both_powers_data, "thread count", "power [W]", "power consumption heat stencil", ["i7 3770", "R7 5800X"], "upper left")
+    # create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time heat stencil", ["i7 3770", "R7 5800X"], "upper right")
 
 
-def _create_heat_stencil_plots_r7_grouping_by_threads():
-    folder_name = "R7-5800X"
-    parameters = get_config(folder_name)
+def _create_heat_stencil_plots_by_frequencies():
     benchmark_name = "heat-stencil"
-    grouping_metric = parameters.thread_counts
 
-    energies, times = process(parameters, benchmark_name, folder_name, grouping_metric, frequency=2200)
-    powers = [energy / time for energy, time in zip(energies, times)]
+    parameters = get_config("i7-3770")
+    grouping_metric = parameters.limits
+    i7_energies, i7_times = process(parameters, benchmark_name, "i7-3770", grouping_metric, thread_count=4)
+    i7_powers = [energy / time for energy, time in zip(i7_energies, i7_times)]
 
-    energies_data = [(grouping_metric, energies)]
-    times_data = [(grouping_metric, times)]
-    powers_data = [(grouping_metric, powers)]
-    energies_times_data = [(times, energies)]
+    i7_energies_data = [(grouping_metric, i7_energies)]
+    i7_times_data = [(grouping_metric, i7_times)]
+    i7_powers_data = [(grouping_metric, i7_powers)]
+    i7_energies_times_data = [(i7_times, i7_energies)]
 
-    # create_scatter_plot(r7_energies_data, "frequencies", "energy [J]", "energy consumption heat stencil", ["R7 5800X"], "upper left")
-    # create_scatter_plot(times_data, "frequencies", "time", "time heat stencil", folder_name)
-    # create_scatter_plot(powers_data, "frequencies", "power", "power consumption heat stencil", folder_name)
-    # create_scatter_plot(energies_times_data, "times", "energies", "energy/time heat stencil", folder_name)
+    r7_parameters = get_config("R7-5800X")
+    r7_grouping_metric = r7_parameters.limits
+    r7_energies, r7_times = process(r7_parameters, benchmark_name, "R7-5800X", r7_grouping_metric, thread_count=4)
+    r7_powers = [energy / time for energy, time in zip(r7_energies, r7_times)]
+
+    r7_energies_data = [(r7_grouping_metric, r7_energies)]
+    r7_times_data = [(r7_grouping_metric, r7_times)]
+    r7_powers_data = [(grouping_metric, r7_powers)]
+    r7_energies_times_data = [(r7_times, r7_energies)]
+
+    both_energies_data = [(grouping_metric, i7_energies), (r7_grouping_metric, r7_energies)]
+    both_times_data = [(grouping_metric, i7_times), (r7_grouping_metric, r7_times)]
+    both_powers_data = [(grouping_metric, i7_powers), (r7_grouping_metric, r7_powers)]
+    both_energies_times_data = [(i7_times, i7_energies), (r7_times, r7_energies)]
+
+    create_scatter_plot(both_energies_data, "frequency [MHz]", "energy [J]", "energy consumption heat stencil", ["i7 3770", "R7 5800X"], "upper left")
+    create_scatter_plot(both_times_data, "frequency [MHz]", "time [s]", "time heat stencil", ["i7 3770", "R7 5800X"], "upper right")
+    create_scatter_plot(both_powers_data, "frequency [MHz]", "power [W]", "power consumption heat stencil", ["i7 3770", "R7 5800X"], "upper left")
+    # create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time heat stencil", ["i7 3770", "R7 5800X"], "upper right")
 
 
-    create_scatter_plot(powers_data, "thread count", "power consumption [W]", "power consumption heat stencil", ["R7 5800X - 2200MHz"], "upper left")
-    create_scatter_plot(times_data, "thread count", "time [s]", "time heat stencil", ["R7 5800X - 2200MHz"], "upper right")
-    create_scatter_plot(energies_data, "thread count", "energy consumption [J]", "energy heat stencil", ["R7 5800X - 2200MHz"], "upper right")
-    #create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time heat stencil", ["i7 3770", "R7 5800X"], "upper left")
+def _create_heat_stencil_plots_by_problem_size():
+    benchmark_name = "heat-stencil"
+
+    parameters = get_config("i7-3770")
+    grouping_metric = parameters.map_sizes
+    i7_energies, i7_times = process(parameters, benchmark_name, "i7-3770", grouping_metric, thread_count=4, frequency=2100)
+    i7_powers = [energy / time for energy, time in zip(i7_energies, i7_times)]
+
+    i7_energies_data = [(grouping_metric, i7_energies)]
+    i7_times_data = [(grouping_metric, i7_times)]
+    i7_powers_data = [(grouping_metric, i7_powers)]
+    i7_energies_times_data = [(i7_times, i7_energies)]
+
+    r7_parameters = get_config("R7-5800X")
+    r7_grouping_metric = r7_parameters.map_sizes
+    r7_energies, r7_times = process(r7_parameters, benchmark_name, "R7-5800X", r7_grouping_metric, thread_count=4, frequency=2200)
+    r7_powers = [energy / time for energy, time in zip(r7_energies, r7_times)]
+
+    r7_energies_data = [(r7_grouping_metric, r7_energies)]
+    r7_times_data = [(r7_grouping_metric, r7_times)]
+    r7_powers_data = [(grouping_metric, r7_powers)]
+    r7_energies_times_data = [(r7_times, r7_energies)]
+
+    both_energies_data = [(grouping_metric, i7_energies), (r7_grouping_metric, r7_energies)]
+    both_times_data = [(grouping_metric, i7_times), (r7_grouping_metric, r7_times)]
+    both_powers_data = [(grouping_metric, i7_powers), (r7_grouping_metric, r7_powers)]
+    both_energies_times_data = [(i7_times, i7_energies), (r7_times, r7_energies)]
+
+    create_scatter_plot(both_energies_data, "frequency [MHz]", "energy [J]", "energy consumption heat stencil", ["i7 3770", "R7 5800X"], "upper left")
+    create_scatter_plot(both_times_data, "frequency [MHz]", "time [s]", "time heat stencil", ["i7 3770", "R7 5800X"], "upper right")
+    create_scatter_plot(both_powers_data, "frequency [MHz]", "power [W]", "power consumption heat stencil", ["i7 3770", "R7 5800X"], "upper left")
+    # create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time heat stencil", ["i7 3770", "R7 5800X"], "upper right")
+
+
+def _create_heat_stencil_plots_by_optimization():
+    benchmark_name = "heat-stencil"
+
+    parameters = get_config("i7-3770")
+    grouping_metric = parameters.optimization_flags
+    i7_energies, i7_times = process(parameters, benchmark_name, "i7-3770", grouping_metric, thread_count=4, frequency=2100)
+    i7_powers = [energy / time for energy, time in zip(i7_energies, i7_times)]
+
+    i7_energies_data = [(grouping_metric, i7_energies)]
+    i7_times_data = [(grouping_metric, i7_times)]
+    i7_powers_data = [(grouping_metric, i7_powers)]
+    i7_energies_times_data = [(i7_times, i7_energies)]
+
+    r7_parameters = get_config("R7-5800X")
+    r7_grouping_metric = r7_parameters.optimization_flags
+    r7_energies, r7_times = process(r7_parameters, benchmark_name, "R7-5800X", r7_grouping_metric, thread_count=4, frequency=2200)
+    r7_powers = [energy / time for energy, time in zip(r7_energies, r7_times)]
+
+    r7_energies_data = [(r7_grouping_metric, r7_energies)]
+    r7_times_data = [(r7_grouping_metric, r7_times)]
+    r7_powers_data = [(grouping_metric, r7_powers)]
+    r7_energies_times_data = [(r7_times, r7_energies)]
+
+    both_energies_data = [(grouping_metric, i7_energies), (r7_grouping_metric, r7_energies)]
+    both_times_data = [(grouping_metric, i7_times), (r7_grouping_metric, r7_times)]
+    both_powers_data = [(grouping_metric, i7_powers), (r7_grouping_metric, r7_powers)]
+    both_energies_times_data = [(i7_times, i7_energies), (r7_times, r7_energies)]
+
+    create_scatter_plot(both_energies_data, "frequency [MHz]", "energy [J]", "energy consumption heat stencil", ["i7 3770", "R7 5800X"], "upper right")
+    create_scatter_plot(both_times_data, "frequency [MHz]", "time [s]", "time heat stencil", ["i7 3770", "R7 5800X"], "upper right")
+    create_scatter_plot(both_powers_data, "frequency [MHz]", "power [W]", "power consumption heat stencil", ["i7 3770", "R7 5800X"], "center left")
+    # create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time heat stencil", ["i7 3770", "R7 5800X"], "upper right")
 
 
 if __name__ == "__main__":
-    _create_heat_stencil_plots_r7_grouping_by_threads()
+    _create_heat_stencil_plots_by_optimization()
--- benchmarking/code/plotting/monte_carlo_plotting.py
+++ benchmarking/code/plotting/monte_carlo_plotting.py
@@ -2,23 +2,18 @@ from data_processing import process, get_config
 from plotting_templates import create_bar_plot, create_heatmap, create_scatter_plot
 
 
-def _create_monte_carlo_plots():
-    folder_name = "i7-3770"
-    parameters = get_config(folder_name)
+def _create_monte_carlo_plots_by_thread_counts():
     benchmark_name = "monte-carlo"
+
+    parameters = get_config("i7-3770")
     grouping_metric = parameters.thread_counts
-    energies, times = process(parameters, benchmark_name, folder_name, grouping_metric, frequency=2100)
-    powers = [energy / time for energy, time in zip(energies, times)]
+    i7_energies, i7_times = process(parameters, benchmark_name, "i7-3770", grouping_metric, frequency=2100)
+    i7_powers = [energy / time for energy, time in zip(i7_energies, i7_times)]
 
-    energies_data = [(grouping_metric, energies)]
-    times_data = [(grouping_metric, times)]
-    powers_data = [(grouping_metric, powers)]
-    energies_times_data = [(times, energies)]
-
-    #create_scatter_plot(energies_data, "frequencies", "energy [J]", "energy consumption monte carlo", folder_name)
-    #create_scatter_plot(times_data, "frequencies", "time", "time monte carlo", folder_name)
-    #create_scatter_plot(powers_data, "frequencies", "power", "power consumption monte carlo", folder_name)
-    #create_scatter_plot(energies_times_data, "times", "energies", "energy/time monte carlo", folder_name)
+    i7_energies_data = [(grouping_metric, i7_energies)]
+    i7_times_data = [(grouping_metric, i7_times)]
+    i7_powers_data = [(grouping_metric, i7_powers)]
+    i7_energies_times_data = [(i7_times, i7_energies)]
 
     r7_parameters = get_config("R7-5800X")
     r7_grouping_metric = r7_parameters.thread_counts
@@ -27,22 +22,121 @@ def _create_monte_carlo_plots():
 
     r7_energies_data = [(r7_grouping_metric, r7_energies)]
     r7_times_data = [(r7_grouping_metric, r7_times)]
+    r7_powers_data = [(grouping_metric, r7_powers)]
+    r7_energies_times_data = [(r7_times, r7_energies)]
 
-    # create_scatter_plot(r7_energies_data, "frequencies", "energy [J]", "energy consumption monte carlo", ["R7 5800X"], "upper left")
-    # create_scatter_plot(times_data, "frequencies", "time", "time monte carlo", folder_name)
-    # create_scatter_plot(powers_data, "frequencies", "power", "power consumption monte carlo", folder_name)
-    # create_scatter_plot(energies_times_data, "times", "energies", "energy/time monte carlo", folder_name)
+    both_energies_data = [(grouping_metric, i7_energies), (r7_grouping_metric, r7_energies)]
+    both_times_data = [(grouping_metric, i7_times), (r7_grouping_metric, r7_times)]
+    both_powers_data = [(grouping_metric, i7_powers), (r7_grouping_metric, r7_powers)]
+    both_energies_times_data = [(i7_times, i7_energies), (r7_times, r7_energies)]
 
-    both_energies_data = [(grouping_metric, energies), (r7_grouping_metric, r7_energies)]
-    both_times_data = [(grouping_metric, times), (r7_grouping_metric, r7_times)]
-    both_powers_data = [(grouping_metric, powers), (r7_grouping_metric, r7_powers)]
-    both_energies_times_data = [(times, energies), (r7_times, r7_energies)]
+    create_scatter_plot(both_energies_data, "thread_count", "energy [J]", "energy consumption monte carlo", ["i7 3770", "R7 5800X"], "upper right")
+    create_scatter_plot(both_times_data, "thread count", "time [s]", "time monte carlo", ["i7 3770", "R7 5800X"], "upper right")
+    create_scatter_plot(both_powers_data, "thread count", "power [W]", "power consumption monte carlo", ["i7 3770", "R7 5800X"], "upper left")
+    # create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time monte carlo", ["i7 3770", "R7 5800X"], "upper right")
 
-    #create_scatter_plot(both_energies_data, "frequencies [MHz]", "energy [J]", "energy consumption monte carlo", ["i7 3770", "R7 5800X"], "upper left")
-    #create_scatter_plot(both_times_data, "frequencies [MHz]", "time [s]", "time monte carlo", ["i7 3770", "R7 5800X"], "upper right")
-    #create_scatter_plot(both_powers_data, "times [s]", "power [W]", "power monte carlo", ["i7 3770", "R7 5800X"], "upper left")
-    create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time monte carlo", ["i7 3770", "R7 5800X"], "upper left")
+
+def _create_monte_carlo_plots_by_frequencies():
+    benchmark_name = "monte-carlo"
+
+    parameters = get_config("i7-3770")
+    grouping_metric = parameters.limits
+    i7_energies, i7_times = process(parameters, benchmark_name, "i7-3770", grouping_metric, thread_count=4)
+    i7_powers = [energy / time for energy, time in zip(i7_energies, i7_times)]
+
+    i7_energies_data = [(grouping_metric, i7_energies)]
+    i7_times_data = [(grouping_metric, i7_times)]
+    i7_powers_data = [(grouping_metric, i7_powers)]
+    i7_energies_times_data = [(i7_times, i7_energies)]
+
+    r7_parameters = get_config("R7-5800X")
+    r7_grouping_metric = r7_parameters.limits
+    r7_energies, r7_times = process(r7_parameters, benchmark_name, "R7-5800X", r7_grouping_metric, thread_count=4)
+    r7_powers = [energy / time for energy, time in zip(r7_energies, r7_times)]
+
+    r7_energies_data = [(r7_grouping_metric, r7_energies)]
+    r7_times_data = [(r7_grouping_metric, r7_times)]
+    r7_powers_data = [(grouping_metric, r7_powers)]
+    r7_energies_times_data = [(r7_times, r7_energies)]
+
+    both_energies_data = [(grouping_metric, i7_energies), (r7_grouping_metric, r7_energies)]
+    both_times_data = [(grouping_metric, i7_times), (r7_grouping_metric, r7_times)]
+    both_powers_data = [(grouping_metric, i7_powers), (r7_grouping_metric, r7_powers)]
+    both_energies_times_data = [(i7_times, i7_energies), (r7_times, r7_energies)]
+
+    create_scatter_plot(both_energies_data, "frequency [MHz]", "energy [J]", "energy consumption monte carlo", ["i7 3770", "R7 5800X"], "upper left")
+    create_scatter_plot(both_times_data, "frequency [MHz]", "time [s]", "time monte carlo", ["i7 3770", "R7 5800X"], "upper right")
+    create_scatter_plot(both_powers_data, "frequency [MHz]", "power [W]", "power consumption monte carlo", ["i7 3770", "R7 5800X"], "upper left")
+    # create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time monte carlo", ["i7 3770", "R7 5800X"], "upper right")
+
+
+def _create_monte_carlo_plots_by_problem_size():
+    benchmark_name = "monte-carlo"
+
+    parameters = get_config("i7-3770")
+    grouping_metric = parameters.map_sizes
+    i7_energies, i7_times = process(parameters, benchmark_name, "i7-3770", grouping_metric, thread_count=4, frequency=2100)
+    i7_powers = [energy / time for energy, time in zip(i7_energies, i7_times)]
+
+    i7_energies_data = [(grouping_metric, i7_energies)]
+    i7_times_data = [(grouping_metric, i7_times)]
+    i7_powers_data = [(grouping_metric, i7_powers)]
+    i7_energies_times_data = [(i7_times, i7_energies)]
+
+    r7_parameters = get_config("R7-5800X")
+    r7_grouping_metric = r7_parameters.map_sizes
+    r7_energies, r7_times = process(r7_parameters, benchmark_name, "R7-5800X", r7_grouping_metric, thread_count=4, frequency=2200)
+    r7_powers = [energy / time for energy, time in zip(r7_energies, r7_times)]
+
+    r7_energies_data = [(r7_grouping_metric, r7_energies)]
+    r7_times_data = [(r7_grouping_metric, r7_times)]
+    r7_powers_data = [(grouping_metric, r7_powers)]
+    r7_energies_times_data = [(r7_times, r7_energies)]
+
+    both_energies_data = [(grouping_metric, i7_energies), (r7_grouping_metric, r7_energies)]
+    both_times_data = [(grouping_metric, i7_times), (r7_grouping_metric, r7_times)]
+    both_powers_data = [(grouping_metric, i7_powers), (r7_grouping_metric, r7_powers)]
+    both_energies_times_data = [(i7_times, i7_energies), (r7_times, r7_energies)]
+
+    create_scatter_plot(both_energies_data, "frequency [MHz]", "energy [J]", "energy consumption monte carlo", ["i7 3770", "R7 5800X"], "upper left")
+    create_scatter_plot(both_times_data, "frequency [MHz]", "time [s]", "time monte carlo", ["i7 3770", "R7 5800X"], "upper left")
+    create_scatter_plot(both_powers_data, "frequency [MHz]", "power [W]", "power consumption monte carlo", ["i7 3770", "R7 5800X"], "center left")
+    # create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time monte carlo", ["i7 3770", "R7 5800X"], "upper right")
+
+
+def _create_monte_carlo_plots_by_optimization():
+    benchmark_name = "monte-carlo"
+
+    parameters = get_config("i7-3770")
+    grouping_metric = parameters.optimization_flags
+    i7_energies, i7_times = process(parameters, benchmark_name, "i7-3770", grouping_metric, thread_count=4, frequency=2100)
+    i7_powers = [energy / time for energy, time in zip(i7_energies, i7_times)]
+
+    i7_energies_data = [(grouping_metric, i7_energies)]
+    i7_times_data = [(grouping_metric, i7_times)]
+    i7_powers_data = [(grouping_metric, i7_powers)]
+    i7_energies_times_data = [(i7_times, i7_energies)]
+
+    r7_parameters = get_config("R7-5800X")
+    r7_grouping_metric = r7_parameters.optimization_flags
+    r7_energies, r7_times = process(r7_parameters, benchmark_name, "R7-5800X", r7_grouping_metric, thread_count=4, frequency=2200)
+    r7_powers = [energy / time for energy, time in zip(r7_energies, r7_times)]
+
+    r7_energies_data = [(r7_grouping_metric, r7_energies)]
+    r7_times_data = [(r7_grouping_metric, r7_times)]
+    r7_powers_data = [(grouping_metric, r7_powers)]
+    r7_energies_times_data = [(r7_times, r7_energies)]
+
+    both_energies_data = [(grouping_metric, i7_energies), (r7_grouping_metric, r7_energies)]
+    both_times_data = [(grouping_metric, i7_times), (r7_grouping_metric, r7_times)]
+    both_powers_data = [(grouping_metric, i7_powers), (r7_grouping_metric, r7_powers)]
+    both_energies_times_data = [(i7_times, i7_energies), (r7_times, r7_energies)]
+
+    create_scatter_plot(both_energies_data, "frequency [MHz]", "energy [J]", "energy consumption monte carlo", ["i7 3770", "R7 5800X"], "center right")
+    create_scatter_plot(both_times_data, "frequency [MHz]", "time [s]", "time monte carlo", ["i7 3770", "R7 5800X"], "upper right")
+    create_scatter_plot(both_powers_data, "frequency [MHz]", "power [W]", "power consumption monte carlo", ["i7 3770", "R7 5800X"], "center left")
+    # create_scatter_plot(both_energies_times_data, "times", "energies", "energy/time monte carlo", ["i7 3770", "R7 5800X"], "upper right")
 
 
 if __name__ == "__main__":
-    _create_monte_carlo_plots()
+    _create_monte_carlo_plots_by_optimization()
